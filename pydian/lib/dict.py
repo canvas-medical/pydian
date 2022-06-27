d015b293295db14d1e1588918b782b4bf8b5464d
ericpan64 <eric.pan@canvasmedical.com>
2022-06-27 10:59:53 -0400

    Add [*] handling in nested_delete

--- pydian/lib/dict.py
+++ pydian/lib/dict.py
@@ -6,33 +6,42 @@ from .enums import RelativeObjectLevel as ROL
 from benedict import benedict
 from benedict.dicts.keypath import keypath_util
 
-def get(msg: dict, key: str, default: Any = None, then: Callable | None = None, drop_rol: ROL | None = None):
-    res = nested_get(msg, key, default) \
-        if '.' in key else single_get(msg, key, default)
+
+def get(
+    msg: dict,
+    key: str,
+    default: Any = None,
+    then: Callable | None = None,
+    drop_rol: ROL | None = None,
+):
+    res = nested_get(msg, key, default) if "." in key else single_get(msg, key, default)
     if res and callable(then):
         try:
             res = then(res)
         except Exception as e:
-            raise RuntimeError(f'`then` callable failed when getting key: {key}, error: {e}')
+            raise RuntimeError(
+                f"`then` callable failed when getting key: {key}, error: {e}"
+            )
     if drop_rol and res is None:
         res = drop_rol
     return res
+
 
 def single_get(msg: dict, key: str, default: Any = None) -> Any:
     """
     Gets single item, supports int indexing, e.g. `someKey[0]`
     """
     res = msg
-    idx = re.search(r'\[\d+\]', key)
+    idx = re.search(r"\[\d+\]", key)
     if idx:
         # TODO: consolidate str logic into shared functions
-        #       E.g. have `_clean_idx` handle this case 
+        #       E.g. have `_clean_idx` handle this case
         idx_str = idx.group(0)
-        i = int(idx_str[1:-1]) # Casts str->int, e.g. "[0]" -> 0
-        key = key.replace(idx_str, '')
+        i = int(idx_str[1:-1])  # Casts str->int, e.g. "[0]" -> 0
+        key = key.replace(idx_str, "")
         res = res.get(key, [])
         res = res[i] if i in range(len(res)) else None
-    elif key[-3:] == '[*]':
+    elif key[-3:] == "[*]":
         res = res.get(key[:-3])
         res = _handle_ending_star_unwrap(res, key)
     else:
@@ -51,22 +60,22 @@ def nested_get(msg: dict, key: str, default: Any = None) -> Any:
     If d[*] is passed, then that means return a list of all elements. E.g. for a dict d:
     d[*].a.b
     will try to get e['a']['b'] for e in d
-    
+
     TODO: Add support for querying, maybe e.g. [?: thing.val==1]
     """
-    if '.' not in key:
+    if "." not in key:
         return single_get(msg, key, default)
-    stack = key.split('.')
+    stack = key.split(".")
     res = deepcopy(msg)
     while len(stack) > 0:
         k = stack.pop(0)
-        # If need to unwrap, then empty stack 
-        if k[-3:] == '[*]':
+        # If need to unwrap, then empty stack
+        if k[-3:] == "[*]":
             k = k[:-3]
-            remaining_key = '.'.join(stack)
-            stack = [] # wipe stack for current run
+            remaining_key = ".".join(stack)
+            stack = []  # wipe stack for current run
             res = res.get(k)
-            if remaining_key != '':
+            if remaining_key != "":
                 res = [nested_get(v, remaining_key, None) for v in res]
         else:
             res = single_get(res, k)
@@ -74,6 +83,7 @@ def nested_get(msg: dict, key: str, default: Any = None) -> Any:
             break
     res = _handle_ending_star_unwrap(res, key)
     return res if res != None else default
+
 
 def nested_delete(msg: benedict, key: str) -> dict:
     """
@@ -83,25 +93,36 @@ def nested_delete(msg: benedict, key: str) -> dict:
     res = deepcopy(msg)
     if type(res) == dict:
         res = benedict(res)
-    nesting = keypath_util.parse_keys(key, '.')
+    nestings = [keypath_util.parse_keys(k, ".") for k in key.split("[*].")]
     # Case: value has an ROL object
     v = get(res, key)
     if type(v) == ROL:
         assert v.value < 0
-        nesting = nesting[:v.value]
+        nestings[-1] = nestings[-1][: v.value]
     # Get up to the last key in nesting, then set that key to None
     #  We set to None instead of popping to preserve indices
     try:
-        # TODO: Handle [*] case
-        res[nesting] = None
+        curr_nesting = nestings[0]
+        if len(nestings) > 1:
+            remaining_key = ".".join(key.split("[*].", 1)[1:])
+            assert issubclass(type(res[curr_nesting][0]), dict)
+            res[curr_nesting] = [
+                nested_delete(r, remaining_key) if issubclass(type(r), dict) else None
+                for r in res[curr_nesting]
+            ]
+        else:
+            res[curr_nesting] = None
     except Exception as e:
-        raise IndexError(f'Failed to perform nested_delete on key: {key}, Error: {e}, Input: {msg}')
+        raise IndexError(
+            f"Failed to perform nested_delete on key: {key}, Error: {e}, Input: {msg}"
+        )
     return res
+
 
 def _handle_ending_star_unwrap(res: dict, key: str) -> dict:
     # HACK: Handle unwrapping if specified at the end
     # TODO: Find a nicer way to do this. Works for now...
-    if key[-3:] == '[*]' and type(res) == list and type(res[0]) == list:
+    if key[-3:] == "[*]" and type(res) == list and type(res[0]) == list:
         res = [l for l in res if l != None]
         res = list(chain(*res))
     return res
--- tests/lib/test_dict.py
+++ tests/lib/test_dict.py
@@ -3,71 +3,72 @@ from pydian.lib.dict import nested_delete
 from pydian import get
 from copy import deepcopy
 
+
 @pytest.fixture
 def nested_data() -> dict:
     return {
-        'const_data': 123,
-        'nested_data': {
-            'a': {
-                'b': 'c',
-                'd': 'e'
-            }
-        },
-        'list_data': [{
-            'patient': {
-                'id': 'abc123',
-                'active': True,
-                'ints': [1, 2, 3],
-                'dicts': [
-                    {'num': 1},
-                    {'num': 2}
-                ]
-            }
-        },
-        {
-            'patient': {
-                'id': 'def456',
-                'active': False,
-                'ints': [4, 5, 6],
-                'dicts': [
-                    {'num': 3},
-                    {'num': 4}
-                ]
-            }
-        },
-        {
-            'patient': {
-                'id': 'ghi789',
-                'active': True,
-                'ints': [7, 8, 9],
-                'dicts': [
-                    {'num': 5},
-                    {'num': 6}
-                ]
-            }
-        },
-        {
-            'patient': {
-                'id': 'jkl101112',
-                'active': True,
-                # 'ints' is deliberately missing
-                'dicts': [
-                    {'num': 7}
-                ]
-            }
-        },
-        ]
+        "const_data": 123,
+        "nested_data": {"a": {"b": "c", "d": "e"}},
+        "list_data": [
+            {
+                "patient": {
+                    "id": "abc123",
+                    "active": True,
+                    "ints": [1, 2, 3],
+                    "dicts": [{"num": 1}, {"num": 2}],
+                }
+            },
+            {
+                "patient": {
+                    "id": "def456",
+                    "active": False,
+                    "ints": [4, 5, 6],
+                    "dicts": [{"num": 3}, {"num": 4}],
+                }
+            },
+            {
+                "patient": {
+                    "id": "ghi789",
+                    "active": True,
+                    "ints": [7, 8, 9],
+                    "dicts": [{"num": 5}, {"num": 6}],
+                }
+            },
+            {
+                "patient": {
+                    "id": "jkl101112",
+                    "active": True,
+                    # 'ints' is deliberately missing
+                    "dicts": [{"num": 7}],
+                }
+            },
+        ],
     }
+
 
 def test_nested_delete(nested_data: dict) -> dict:
     orig = nested_data
     source = deepcopy(nested_data)
     keys = (
-        'list_data[0].patient',
-        'list_data[2].patient.id',
-        'list_data[3].patient.active'
+        "list_data[0].patient",
+        "list_data[2].patient.id",
+        "list_data[3].patient.active",
     )
     for k in keys:
         res = nested_delete(source, k)
         assert get(res, k) == None
         assert get(orig, k) != None
+    # Test [*] format
+    source = deepcopy(nested_data)
+    single_star_keys = ("list_data[*].patient.dicts",)
+    for k in single_star_keys:
+        res = nested_delete(source, k)
+        assert len(get(res, k)) == len(get(orig, k))
+        assert not any(get(res, k))
+        assert any(get(orig, k))
+    double_star_keys = ("list_data[*].patient.dicts[*].num",)
+    for k in double_star_keys:
+        res = nested_delete(source, k)
+        assert len(get(res, k)) == len(get(orig, k))
+        assert not any([any(l) for l in get(res, k)])
+        assert any([any(l) for l in get(orig, k)])
