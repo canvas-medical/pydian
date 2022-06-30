d966d7e50fcc7d5e95db1f388437db7e5dddf871
ericpan64 <eric.pan@canvasmedical.com>
2022-06-30 11:32:56 -0400

    Simplify `_nested_get`
    Leverage existing keypath support from benedict

--- pydian/lib/dict.py
+++ pydian/lib/dict.py
@@ -14,11 +14,7 @@ def get(
     apply: Callable | None = None,
     drop_level: ROL | None = None,
 ):
-    res = (
-        _nested_get(source, key, default)
-        if "." in key
-        else _single_get(source, key, default)
-    )
+    res = _nested_get(source, key, default)
     if res and apply:
         try:
             res = apply(res)
@@ -29,29 +25,6 @@ def get(
     if drop_level and res is None:
         res = drop_level
     return res
-
-
-def _single_get(source: dict, key: str, default: Any = None) -> Any:
-    """
-    Gets single item, supports int indexing, e.g. `someKey[0]`
-    """
-    res = source
-    idx = re.search(r"\[\d+\]", key)
-    if idx:
-        # TODO: consolidate str logic into shared functions
-        #       E.g. have `_clean_idx` handle this case
-        idx_str = idx.group(0)
-        i = int(idx_str[1:-1])  # Casts str->int, e.g. "[0]" -> 0
-        key = key.replace(idx_str, "")
-        res = res.get(key, [])
-        res = res[i] if i in range(len(res)) else None
-    elif key.endswith("[*]"):
-        res = res.get(key[:-3])
-        res = __handle_ending_star_unwrap(res, key)
-    else:
-        res = res.get(key, default)
-    return res
-
 
 def _nested_get(source: dict, key: str, default: Any = None) -> Any:
     """
@@ -67,26 +40,16 @@ def _nested_get(source: dict, key: str, default: Any = None) -> Any:
 
     TODO: Add support for querying, maybe e.g. [?: thing.val==1]
     """
-    if "." not in key:
-        return _single_get(source, key, default)
-    stack = key.split(".")
-    res = deepcopy(source)
-    while len(stack) > 0:
-        k = stack.pop(0)
-        # If need to unwrap, then empty stack
-        if k.endswith("[*]"):
-            k = k[:-3]
-            remaining_key = ".".join(stack)
-            stack = []  # wipe stack for current run
-            res = res.get(k)
-            if remaining_key != "":
-                res = [_nested_get(v, remaining_key, None) for v in res]
-        else:
-            res = _single_get(res, k)
-        if res == None:
-            break
+    res = deepcopy(benedict(source))
+    keypaths = key.split("[*].", 1)
+    if '[*]' in keypaths[0]:
+        res = res.get(keypaths[0][:-3])
+    else:
+        res = res.get(keypaths[0])
+    if len(keypaths) > 1:
+        res = [_nested_get(v, keypaths[1]) for v in res]
     res = __handle_ending_star_unwrap(res, key)
-    return res if res != None else default
+    return res if res is not None else default
 
 
 def _nested_delete(source: dict, key: str) -> dict:
@@ -98,7 +61,7 @@ def _nested_delete(source: dict, key: str) -> dict:
     keypaths = [keypath_util.parse_keys(k, ".") for k in key.split("[*].")]
     # Case: value has an ROL object
     v = get(res, key)
-    if type(v) == ROL:
+    if isinstance(v, ROL):
         assert v.value < 0
         keypaths[-1] = keypaths[-1][: v.value]
     # Get up to the last key in keypath, then set that key to None
@@ -107,9 +70,9 @@ def _nested_delete(source: dict, key: str) -> dict:
         curr_keypath = keypaths[0]
         if len(keypaths) > 1:
             remaining_key = ".".join(key.split("[*].", 1)[1:])
-            assert issubclass(type(res[curr_keypath][0]), dict)
+            assert isinstance(res[curr_keypath][0], dict)
             res[curr_keypath] = [
-                _nested_delete(r, remaining_key) if issubclass(type(r), dict) else None
+                _nested_delete(r, remaining_key) if isinstance(r, dict) else None
                 for r in res[curr_keypath]
             ]
         else:
