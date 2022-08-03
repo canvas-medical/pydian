a54a21cf64bf452fbd4509e0463d7d8036531f50
ericpan64 <eric.pan@canvasmedical.com>
2022-08-03 13:46:48 -0400

    Move shared fixtures to `conftest.py`

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,61 +1,17 @@
 from copy import deepcopy
 from typing import Any
 
-import pytest
-
 from pydian import get
 from pydian.dicts import _nested_delete
-
-
-@pytest.fixture
-def nested_data() -> dict[str, Any]:
-    return {
-        "const_data": 123,
-        "nested_data": {"a": {"b": "c", "d": "e"}},
-        "list_data": [
-            {
-                "patient": {
-                    "id": "abc123",
-                    "active": True,
-                    "ints": [1, 2, 3],
-                    "dicts": [{"num": 1}, {"num": 2}],
-                }
-            },
-            {
-                "patient": {
-                    "id": "def456",
-                    "active": False,
-                    "ints": [4, 5, 6],
-                    "dicts": [{"num": 3}, {"num": 4}],
-                }
-            },
-            {
-                "patient": {
-                    "id": "ghi789",
-                    "active": True,
-                    "ints": [7, 8, 9],
-                    "dicts": [{"num": 5}, {"num": 6}],
-                }
-            },
-            {
-                "patient": {
-                    "id": "jkl101112",
-                    "active": True,
-                    # "ints" is deliberately missing
-                    "dicts": [{"num": 7}],
-                }
-            },
-        ],
-    }
 
 
 def test_nested_delete(nested_data: dict[str, Any]) -> None:
     orig = nested_data
     source = deepcopy(nested_data)
     keys_to_drop = {
-        "list_data[0].patient",
-        "list_data[2].patient.id",
-        "list_data[3].patient.active",
+        "data[0].patient",
+        "data[2].patient.id",
+        "data[3].patient.active",
     }
 
     res = _nested_delete(source, keys_to_drop)
--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -5,57 +5,6 @@ import pytest
 from pydian import DROP, Mapper, get
 
 
-@pytest.fixture
-def simple_data() -> dict[str, Any]:
-    return {
-        "data": {"patient": {"id": "abc123", "active": True}},
-        "list_data": [
-            {"patient": {"id": "def456", "active": True}},
-            {"patient": {"id": "ghi789", "active": False}},
-        ],
-    }
-
-
-@pytest.fixture
-def nested_data() -> dict[str, Any]:
-    return {
-        "data": [
-            {
-                "patient": {
-                    "id": "abc123",
-                    "active": True,
-                    "ints": [1, 2, 3],
-                    "dicts": [{"num": 1}, {"num": 2}],
-                }
-            },
-            {
-                "patient": {
-                    "id": "def456",
-                    "active": False,
-                    "ints": [4, 5, 6],
-                    "dicts": [{"num": 3}, {"num": 4}],
-                }
-            },
-            {
-                "patient": {
-                    "id": "ghi789",
-                    "active": True,
-                    "ints": [7, 8, 9],
-                    "dicts": [{"num": 5}, {"num": 6}],
-                }
-            },
-            {
-                "patient": {
-                    "id": "jkl101112",
-                    "active": True,
-                    # 'ints' is deliberately missing
-                    "dicts": [{"num": 7}],
-                }
-            },
-        ]
-    }
-
-
 def test_get(simple_data: dict[str, Any]) -> None:
     source = simple_data
 
