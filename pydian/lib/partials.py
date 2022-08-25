f71bdaf342283be5dd5effc00f88db926fb23c40
ericpan64 <eric.pan@canvasmedical.com>
2022-08-25 09:45:49 -0400

    Add hook into `map`

--- pydian/lib/partials.py
+++ pydian/lib/partials.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Any, Callable, Sequence
+from typing import Any, Callable, Iterable, Sequence, TypeVar
 
 import funcy
 
@@ -11,15 +11,16 @@ def take(n: int) -> Callable[[Sequence[Any]], list[Any]]:
     return partial(funcy.take, n)
 
 
+def map_fn(fn: Callable) -> Callable[[Iterable], Any]:
+    """
+    Partial wrapper for `map`
+    """
+    return partial(map, fn)
+
+
 def str_replace(old: str, new: str) -> Callable[[str], str]:
     """
     Partial wrapper for `str.replace`
     """
+    _str_replace = lambda s: s.replace(old, new)
     return partial(_str_replace, old=old, new=new)
-
-
-def _str_replace(s: str, old: str, new: str) -> str:
-    """
-    A wrapper to allow passing kwargs to `str.replace`
-    """
-    return s.replace(old, new)
