8af0236fc39c0712b3378a2f40bc276fa528b96d
ericpan64 <eric.pan@canvasmedical.com>
2022-09-26 17:46:19 -0400

    Remove unused source data references in DROP tests
    Since these tests are focused on just testing the `DROP` enum and aren't
    concerned about how they get there, we can just place them directly
    where we want to test.

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -12,14 +12,14 @@ def test_drop(simple_data: dict[str, Any]) -> None:
         return {
             "CASE_parent_keep": {
                 "CASE_curr_drop": {
-                    "a": get(m, "notFoundKey", drop_level=DROP.THIS_OBJECT),
+                    "a": DROP.THIS_OBJECT,
                     "b": "someValue",
                 },
-                "CASE_curr_keep": {"id": get(m, "data.patient.id", drop_level=DROP.THIS_OBJECT)},
+                "CASE_curr_keep": {"id": get(m, "data.patient.id")},
             },
-            "CASE_list": [get(m, "notFoundKey", drop_level=DROP.THIS_OBJECT)],
+            "CASE_list": [DROP.THIS_OBJECT],
             "CASE_list_of_objects": [
-                {"a": get(m, "notFoundKey", drop_level=DROP.PARENT), "b": "someValue"},
+                {"a": DROP.PARENT, "b": "someValue"},
                 {"a": "someValue", "b": "someValue"},
             ],
         }
@@ -29,13 +29,13 @@ def test_drop(simple_data: dict[str, Any]) -> None:
     assert res == {"CASE_parent_keep": {"CASE_curr_keep": {"id": get(source, "data.patient.id")}}}
 
 
-def test_drop_out_of_bounds(simple_data: dict[str, Any]) -> None:
-    source = simple_data
+def test_drop_out_of_bounds() -> None:
+    source = {}
 
     def mapping(m: dict[str, Any]) -> dict[str, Any]:
         return {
             "parent": {
-                "CASE_no_grandparent": get(m, "notFoundKey", drop_level=DROP.GREATGRANDPARENT)
+                "CASE_no_grandparent": DROP.GREATGRANDPARENT
             }
         }
 
@@ -44,12 +44,12 @@ def test_drop_out_of_bounds(simple_data: dict[str, Any]) -> None:
         _ = mapper(source)
 
 
-def test_drop_exact_level(simple_data: dict[str, Any]) -> None:
-    source = simple_data
+def test_drop_exact_level() -> None:
+    source = {}
 
     def mapping(m: dict[str, Any]) -> dict[str, Any]:
         return {
-            "parent": {"CASE_has_parent_object": get(m, "notFoundKey", drop_level=DROP.PARENT)},
+            "parent": {"CASE_has_parent_object": DROP.PARENT},
             "other_data": 123,
         }
 
